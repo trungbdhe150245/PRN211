5c07286a787761ea5ba2399f302ce86d979de0b6
area1110 <97070827+nguyenkhanhhuyhe153585@users.noreply.github.com>
2022-03-10 12:51:27 +0700

    ViewTest Thêm getTest trong DAO

--- OTS/ViewTest/FrmViewTest.Designer.cs
+++ OTS/ViewTest/FrmViewTest.Designer.cs
@@ -30,25 +30,30 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtTestID = new System.Windows.Forms.TextBox();
+            this.txtTestCode = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpCreateDate = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.dtpStartTime = new System.Windows.Forms.DateTimePicker();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
             this.label6 = new System.Windows.Forms.Label();
-            this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+            this.dtpDuration = new System.Windows.Forms.DateTimePicker();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.cbReview = new System.Windows.Forms.CheckBox();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtTotalQuestion = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dgvQuestion = new System.Windows.Forms.DataGridView();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuestion)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -70,20 +75,20 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "TestID";
             // 
-            // textBox1
+            // txtTestID
             // 
-            this.textBox1.Location = new System.Drawing.Point(170, 121);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(78, 23);
-            this.textBox1.TabIndex = 2;
+            this.txtTestID.Location = new System.Drawing.Point(170, 121);
+            this.txtTestID.Name = "txtTestID";
+            this.txtTestID.ReadOnly = true;
+            this.txtTestID.Size = new System.Drawing.Size(99, 23);
+            this.txtTestID.TabIndex = 2;
             // 
-            // textBox2
+            // txtTestCode
             // 
-            this.textBox2.Location = new System.Drawing.Point(170, 204);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(263, 23);
-            this.textBox2.TabIndex = 4;
+            this.txtTestCode.Location = new System.Drawing.Point(170, 204);
+            this.txtTestCode.Name = "txtTestCode";
+            this.txtTestCode.Size = new System.Drawing.Size(263, 23);
+            this.txtTestCode.TabIndex = 4;
             // 
             // label3
             // 
@@ -94,17 +99,19 @@
             this.label3.TabIndex = 3;
             this.label3.Text = "Code";
             // 
-            // dateTimePicker1
+            // dtpCreateDate
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(587, 121);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(211, 23);
-            this.dateTimePicker1.TabIndex = 5;
+            this.dtpCreateDate.CustomFormat = "dd/MM/yyyy";
+            this.dtpCreateDate.Enabled = false;
+            this.dtpCreateDate.Location = new System.Drawing.Point(587, 121);
+            this.dtpCreateDate.Name = "dtpCreateDate";
+            this.dtpCreateDate.Size = new System.Drawing.Size(202, 23);
+            this.dtpCreateDate.TabIndex = 5;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(518, 127);
+            this.label4.Location = new System.Drawing.Point(482, 89);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(60, 15);
             this.label4.TabIndex = 6;
@@ -113,28 +120,28 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(524, 168);
+            this.label5.Location = new System.Drawing.Point(474, 47);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.Size = new System.Drawing.Size(68, 15);
             this.label5.TabIndex = 8;
-            this.label5.Text = "Test Date";
+            this.label5.Text = "Create Date";
             // 
-            // dateTimePicker2
+            // dtpStartTime
             // 
-            this.dateTimePicker2.CustomFormat = "yyyy.MM.dd HH:mm";
-            this.dateTimePicker2.Location = new System.Drawing.Point(726, 163);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.ShowUpDown = true;
-            this.dateTimePicker2.Size = new System.Drawing.Size(72, 23);
-            this.dateTimePicker2.TabIndex = 7;
-            this.dateTimePicker2.Value = new System.DateTime(2022, 3, 18, 0, 0, 0, 0);
+            this.dtpStartTime.CustomFormat = "yyyy.MM.dd HH:mm";
+            this.dtpStartTime.Location = new System.Drawing.Point(726, 163);
+            this.dtpStartTime.Name = "dtpStartTime";
+            this.dtpStartTime.ShowUpDown = true;
+            this.dtpStartTime.Size = new System.Drawing.Size(63, 23);
+            this.dtpStartTime.TabIndex = 7;
+            this.dtpStartTime.Value = new System.DateTime(2022, 3, 18, 0, 0, 0, 0);
             // 
-            // dateTimePicker3
+            // dtpStartDate
             // 
-            this.dateTimePicker3.Location = new System.Drawing.Point(587, 163);
-            this.dateTimePicker3.Name = "dateTimePicker3";
-            this.dateTimePicker3.Size = new System.Drawing.Size(133, 23);
-            this.dateTimePicker3.TabIndex = 9;
+            this.dtpStartDate.Location = new System.Drawing.Point(587, 163);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(133, 23);
+            this.dtpStartDate.TabIndex = 9;
             // 
             // label6
             // 
@@ -145,15 +152,15 @@
             this.label6.TabIndex = 10;
             this.label6.Text = "Duration";
             // 
-            // dateTimePicker4
+            // dtpDuration
             // 
-            this.dateTimePicker4.CustomFormat = "yyyy.MM.dd HH:mm";
-            this.dateTimePicker4.Location = new System.Drawing.Point(587, 204);
-            this.dateTimePicker4.Name = "dateTimePicker4";
-            this.dateTimePicker4.ShowUpDown = true;
-            this.dateTimePicker4.Size = new System.Drawing.Size(90, 23);
-            this.dateTimePicker4.TabIndex = 11;
-            this.dateTimePicker4.Value = new System.DateTime(2022, 3, 18, 0, 0, 0, 0);
+            this.dtpDuration.CustomFormat = "yyyy.MM.dd HH:mm";
+            this.dtpDuration.Location = new System.Drawing.Point(587, 204);
+            this.dtpDuration.Name = "dtpDuration";
+            this.dtpDuration.ShowUpDown = true;
+            this.dtpDuration.Size = new System.Drawing.Size(90, 23);
+            this.dtpDuration.TabIndex = 11;
+            this.dtpDuration.Value = new System.DateTime(2022, 3, 18, 0, 0, 0, 0);
             // 
             // label7
             // 
@@ -164,81 +171,132 @@
             this.label7.TabIndex = 12;
             this.label7.Text = "Subject";
             // 
-            // textBox3
+            // txtSubject
             // 
-            this.textBox3.Location = new System.Drawing.Point(170, 163);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(263, 23);
-            this.textBox3.TabIndex = 13;
-            this.textBox3.Text = "asdfjqjwefjakdjnf ajdifjqiehjfmadkjcq jeifjqiejiajfkjqwijfjadkfjoqiejfi";
+            this.txtSubject.Location = new System.Drawing.Point(170, 163);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.ReadOnly = true;
+            this.txtSubject.Size = new System.Drawing.Size(263, 23);
+            this.txtSubject.TabIndex = 13;
             // 
-            // checkBox1
+            // cbReview
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(702, 206);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(87, 19);
-            this.checkBox1.TabIndex = 15;
-            this.checkBox1.Text = "Can Reivew";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.cbReview.AutoSize = true;
+            this.cbReview.Location = new System.Drawing.Point(702, 206);
+            this.cbReview.Name = "cbReview";
+            this.cbReview.Size = new System.Drawing.Size(87, 19);
+            this.cbReview.TabIndex = 15;
+            this.cbReview.Text = "Can Reivew";
+            this.cbReview.UseVisualStyleBackColor = true;
+            this.cbReview.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(264, 124);
+            this.label9.Location = new System.Drawing.Point(263, 44);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(83, 15);
             this.label9.TabIndex = 17;
             this.label9.Text = "Total Question";
             // 
-            // textBox4
+            // txtTotalQuestion
             // 
-            this.textBox4.Location = new System.Drawing.Point(317, 41);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(80, 23);
-            this.textBox4.TabIndex = 18;
+            this.txtTotalQuestion.Location = new System.Drawing.Point(352, 41);
+            this.txtTotalQuestion.Name = "txtTotalQuestion";
+            this.txtTotalQuestion.ReadOnly = true;
+            this.txtTotalQuestion.Size = new System.Drawing.Size(45, 23);
+            this.txtTotalQuestion.TabIndex = 18;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.btnCancel);
+            this.groupBox1.Controls.Add(this.btnCopy);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.txtTotalQuestion);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Location = new System.Drawing.Point(36, 80);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(893, 229);
             this.groupBox1.TabIndex = 19;
             this.groupBox1.TabStop = false;
             // 
-            // dataGridView1
+            // btnCancel
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(36, 341);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(893, 280);
-            this.dataGridView1.TabIndex = 20;
+            this.btnCancel.Location = new System.Drawing.Point(459, 184);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(83, 29);
+            this.btnCancel.TabIndex = 21;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(670, 184);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(83, 29);
+            this.btnCopy.TabIndex = 20;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(776, 184);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(83, 29);
+            this.btnSave.TabIndex = 19;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // dgvQuestion
+            // 
+            this.dgvQuestion.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvQuestion.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvQuestion.Location = new System.Drawing.Point(36, 356);
+            this.dgvQuestion.Name = "dgvQuestion";
+            this.dgvQuestion.RowTemplate.Height = 25;
+            this.dgvQuestion.Size = new System.Drawing.Size(893, 265);
+            this.dgvQuestion.TabIndex = 20;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(567, 184);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(83, 29);
+            this.btnDelete.TabIndex = 22;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(36, 328);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 25);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "Questions";
             // 
             // FrmViewTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(965, 647);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.dgvQuestion);
+            this.Controls.Add(this.cbReview);
+            this.Controls.Add(this.txtSubject);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.dateTimePicker4);
+            this.Controls.Add(this.dtpDuration);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.dateTimePicker3);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.dateTimePicker1);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.dtpStartTime);
+            this.Controls.Add(this.dtpCreateDate);
+            this.Controls.Add(this.txtTestCode);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtTestID);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.groupBox1);
@@ -247,7 +305,7 @@
             this.Load += new System.EventHandler(this.FrmViewTest_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuestion)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -257,22 +315,27 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtTestID;
+        private System.Windows.Forms.TextBox txtTestCode;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpCreateDate;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.DateTimePicker dtpStartTime;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.DateTimePicker dateTimePicker4;
+        private System.Windows.Forms.DateTimePicker dtpDuration;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.CheckBox cbReview;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtTotalQuestion;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvQuestion;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label8;
     }
 }
--- OTS/ViewTest/FrmViewTest.cs
+++ OTS/ViewTest/FrmViewTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OTS.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,20 +13,22 @@ namespace OTS.ViewTest
 {
     public partial class FrmViewTest : Form
     {
-        public FrmViewTest()
+        private Test test;
+        public FrmViewTest(Test test)
         {
+            this.test = test;
             InitializeComponent();
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        public LoadTestInformation()
         {
 
         }
 
         private void FrmViewTest_Load(object sender, EventArgs e)
         {
-            dateTimePicker2.Format = DateTimePickerFormat.Custom;
-            dateTimePicker2.CustomFormat = "HH:mm";
+            dtpStartTime.Format = DateTimePickerFormat.Custom;
+            dtpStartTime.CustomFormat = "HH:mm";
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
