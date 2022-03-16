9421320ca7009f7967e4dd56a27316c409c799bd
NguyenQueTien <tiennqhe153125@fpt.edu.vn>
2022-03-16 17:36:52 +0700

    Start login

---
+++ OTS/Login/FrmLoginStudent.cs
@@ -0,0 +1,57 @@
+﻿using OTS.DAO;
+using OTS.Dashboard;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OTS.Login
+{
+    public partial class FrmLoginStudent : Form
+    {
+        public FrmLoginStudent()
+        {
+            InitializeComponent();
+        }
+        private bool ValidateLogin()
+        {
+            string mess = "";
+            if(txtUsername.Text.Equals(""))
+            {
+                mess = "Username cannot empty";
+            } else if (txtPassword.Text.Equals(""))
+            {
+                mess = "Password cannot empty";
+            }
+
+            if (mess.Equals(""))
+            {
+                return true;
+            } else
+            {
+                MessageBox.Show(mess, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (ValidateLogin())
+            {
+                StudentDBContext dbStudent = new StudentDBContext();
+                if (dbStudent.GetStudent(txtUsername.Text, txtPassword.Text) != null)
+                {
+                    FrmStudentDashboard frmStudentDashboard = new FrmStudentDashboard();
+                    frmStudentDashboard.Show();
+                    this.Hide();
+                }
+            }
+            
+
+        }
+    }
+}
