0109d609edc5e227e25f720224a860d073b67d4d
Nguyen Manh Quy <quynmhe153283@fpt.edu.vn>
2022-03-25 23:36:39 +0700

    Update Program.cs

--- OTS/Program.cs
+++ OTS/Program.cs
@@ -35,9 +35,9 @@ namespace OTS
             //Application.Run(new FrmManageClass());
             //Application.Run(new FrmViewTest(3));
             //Application.Run(new FrmReviewSubmission(34, 8));
-            //Application.Run(new FrmManageMark());
+            Application.Run(new FrmManageMark());
             //Application.Run(new FrmLoginStudent());
-            Application.Run(new FrmEssayTestList(10, 1));
+            //Application.Run(new FrmEssayTestList(10, 1));
             //Application.Run(new TakeTest());
         }
 	}
